78725eec654b6ae6f2312e5e482dd3e5f5f81292
reticulis <reticulis@protonmail.com>
2022-06-29 13:08:03 +0200

    refactor: bump version

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ use std::path::PathBuf;
 #[derive(Parser, Debug)]
 #[clap(
     author = "Writted by reticulis <reticulis@protonmail.com>",
-    version = "0.1.0",
+    version = "0.2.0",
     about = "Create your own simple blog!"
 )]
 struct Cli {
