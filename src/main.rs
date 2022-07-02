35fd435b0718dc62d34c7cda9062ec78f53aba88
reticulis <reticulis@protonmail.com>
2022-07-02 23:53:03 +0200

    chore(release): bump version

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ use std::path::PathBuf;
 #[derive(Parser, Debug)]
 #[clap(
     author = "reticulis <reticulis@protonmail.com>",
-    version = "0.4.0",
+    version = "0.5.0",
     about = "Create your own simple blog!"
 )]
 struct Cli {
